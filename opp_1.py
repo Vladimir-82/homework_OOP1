f80b13ff6a7cbf0b2c61dbb8d7c33d9616bd35ca
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-02-18 13:29:05 +0300

    is_type

--- opp_1.py
+++ opp_1.py
@@ -1,6 +1,6 @@
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 VOVELS = 'aeiouy'
-CONSTANTS = 'bcdfghjklmnpqrstvwxz'
+CONSONANS = 'bcdfghjklmnpqrstvwxz'
 WEIGHT = dict(zip(list(ALPHABET), list(range(1, len(ALPHABET) + 1))))
 
 
@@ -9,37 +9,46 @@ class SomeClass():
     def __init__(self, word):
         self.word = word
 
-    def decision(self):
+    def is_type(self):
         '''
-        Функция возвращает:
-        - Если строка:
-        если произведение гласных и согласных букв меньше или
-        равно длине слова, выводить все гласные, иначе согласные;
-        - Если число:
-        произведение суммы чётных цифр на длину числа.
+        Accepts an object. Returns its type.
+        '''
+        return type(self.word)
+
+    def method_str(self):
+        '''
+        Takes a string. returns the vowels of the string
+        if the multiplication of the letters of the string
+        is less than the length of the string.
+        Else returns the consonants of the string.
         '''
         vovels = []
-        constants = []
+        consonants = []
         mult_string = 1
-        if isinstance(self.word, str):
-            for el in self.word.lower():
-                for k, v in WEIGHT.items():
-                    if el == k:
-                        mult_string *= v
-                        if el in VOVELS:
-                            vovels.append(el)
-                        if el in CONSTANTS:
-                            constants.append(el)
+        for el in self.word.lower():
+            for k, v in WEIGHT.items():
+                if el == k:
+                    mult_string *= v
+                    if el in VOVELS:
+                        vovels.append(el)
+                    if el in CONSONANS:
+                        consonants.append(el)
+        if mult_string <= len(self.word):
+            return vovels
+        else:
+            return consonants
 
-            if mult_string <= len(self.word):
-                return vovels
-            else:
-                return constants
-
-        if isinstance(self.word, int):
-            even_numbers_sum = sum([int(i) for i in list(str(self.word)) if not int(i) % 2])
-            return even_numbers_sum
+    def method_int(self):
+        """
+        Takes a number. Returns the sum of the even digits of a number
+        """
+        even_numbers_sum = sum([int(i) for i in list(str(self.word)) if not int(i) % 2])
+        return even_numbers_sum
 
 
-obj = SomeClass(123)
-print(obj.decision())
+obj = SomeClass('spam')
+method = obj.is_type()
+if method == str:
+    print(obj.method_str())
+if method == int:
+    print(obj.method_int())
